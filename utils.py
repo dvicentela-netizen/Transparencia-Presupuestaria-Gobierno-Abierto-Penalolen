3462fe800274dccab929194a6aa6dbb6729294ff
Dafne Odette Vicentela Becerra <dvicentela@penalolen.cl>
2026-04-22 17:20:13 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,12 +2,5 @@
 import streamlit as st
 
 def sidebar_institucional():
-        st.image("assets/logo.png", width=180)  # ajusta el ancho a tu gusto
-        st.markdown(
-            "<div style='padding:10px 0 18px 0'>"
-            "<span style='font-size:1.1rem;font-weight:700;'>🏛️ Peñalolén</span><br>"
-            "<span style='font-size:0.8rem;opacity:0.7;'>Transparencia Presupuestaria</span>"
-            "</div>",
-            unsafe_allow_html=True,
-        )
+        st.image("assets/logo.png", width=240)  # ajusta el ancho a tu gusto
 
